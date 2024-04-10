ebe7b7b6386bdba1bd58740e40f411536ccbd7df
LiXiaoJun <40384503+LiJunYi2@users.noreply.github.com>
2024-04-10 09:48:43 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,6 +9,13 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
      const url = new URL(request.url);
+     const refererUrl = context.env.REFERER_URL;
+     // 检查Referer头部以验证请求来源
+     const referer = request.headers.get('Referer');
+     if (!referer || !referer.startsWith(refererUrl)) {
+        // 如果Referer不符合预期，返回403 Forbidden或重定向到错误页面
+        return new Response('Access denied', { status: 403 });
+     }
     
     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
          method: request.method,
